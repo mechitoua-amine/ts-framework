a48ea3be43dac8b26cadc17d0748810262b20d40
Mehmet Emin <mehmet@pop-os.localdomain>
2021-09-26 12:55:04 +0100

    Adding Eventing Class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from 'axios';
 
 interface UserProps {
   id?: number;
@@ -6,13 +6,9 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
   constructor(private data: UserProps) {}
-  url = 'http://localhost:3000/users'
-
-  events: {[key: string]: Callback[]} = {};
+  url = 'http://localhost:3000/users';
 
   get(propName: string): number | string {
     return this.data[propName];
@@ -21,31 +17,20 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(event: string, callback: Callback): void {
-    const handlers = this.events[event] || [];
-    handlers.push(callback);
-    this.events[event] = handlers;
-  }
-
-  trigger(event: string): void {
-    const handlers = this.events[event]
-    if(!handlers || handlers.length == 0) return;
-    handlers.forEach(callback => callback());
-  }
-
   fetch(): void {
-    axios.get(`${this.url}/${this.get('id')}`)
-    .then((response: AxiosResponse): void => {
-      this.set(response.data);      
-    })
+    axios
+      .get(`${this.url}/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
   }
 
   save(): void {
-    const id = this.get('id')
-    if(id) {
-      axios.put(`${this.url}/${id}`, this.data)
+    const id = this.get('id');
+    if (id) {
+      axios.put(`${this.url}/${id}`, this.data);
     } else {
-      axios.post(this.url, this.data)
+      axios.post(this.url, this.data);
     }
   }
 }
