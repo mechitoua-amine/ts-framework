13ebba8ede86dfb7212dc62ac8c42b3c676a4df4
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-09-27 20:25:40 +0100

    handling events by adding bindEvents() method

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,6 +1,16 @@
 export class UserFrom {
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      "click:button": this.onButtonClick
+    };
+  }
+
+  onButtonClick(): void {
+    console.log("Button was clicked!");
+  }
+
   template(): string {
     return `
       <div>
@@ -10,9 +20,20 @@ export class UserFrom {
     `;
   }
 
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
   render(): void {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
