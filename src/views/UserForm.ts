145fa53c0c64a8d50a0ac46835157beb9f995591
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-09-27 21:01:05 +0100

    adding setNameClick() event

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,17 +6,24 @@ export class UserFrom {
   }
 
   bindModel(): void {
-    this.model.on('change', () => this.render()
+    this.model.on("change", () => this.render());
   }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      "click:set-age": this.onSetAgeClick
+      "click:set-age": this.onSetAgeClick,
+      "click:set-name": this.onSetNameClick
     };
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector("input");
+    const name = input.value;
+    this.model.set({ name });
   };
 
   template(): string {
@@ -26,7 +33,7 @@ export class UserFrom {
         <div>User Name: ${this.model.get("name")}</div>
         <div>User Age: ${this.model.get("age")}</div>
         <input />
-        <button class="">Click me</button>
+        <button class="set-name">change Name</button>
         <button class="set-age">Set Random Age</button>
       </div>
     `;
@@ -43,7 +50,7 @@ export class UserFrom {
   }
 
   render(): void {
-    this.parent.innerHTML = '';
+    this.parent.innerHTML = "";
 
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
