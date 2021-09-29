d4b4d386f1418f8ffc9f00bc6966da283792aea5
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-09-29 11:12:49 +0100

    Implementig nesting Views insides UserEdit class

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { User } from "./models/User";
 import "./style.css";
-import { UserFrom } from "./views/UserForm";
+import { UserForm } from "./views/UserForm";
 
 const user = User.buildUser({ name: "Name", age: 20 });
 const root = document.getElementById("root");
 if (root) {
-  const userFrom = new UserFrom(root, user);
+  const userFrom = new UserForm(root, user);
   userFrom.render();
 } else {
   throw new Error("Root Element Not Found!");
--- src/views/View.ts
+++ src/views/View.ts
@@ -1,12 +1,19 @@
 import { Model } from "../models/Model";
 
 export abstract class View<T extends Model<K>, K> {
+  regions: { [key: string]: Element } = {};
+
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
 
   abstract template(): string;
+
   eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
+
+  regionsMap(): { [key: string]: string } {
     return {};
   }
 
@@ -24,12 +31,27 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+  mapRegions(fragemnt: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+    for (let key in this.regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragemnt.querySelector(selector);
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
+  onRender(): void {}
+
   render(): void {
     this.parent.innerHTML = "";
 
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+    this.onRender();
     this.parent.append(templateElement.content);
   }
 }
